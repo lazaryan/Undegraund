e634774e065282f7a03531ff6d15adc5ca4f7b24
lazaryan <lazaryan99@gmail.com>
2018-06-18 13:40:24 +0300

    Добавил popup окно для добавления новых посетителей. Жобавил взаимодействие между popup окном и столами

--- src/js/main.js
+++ src/js/main.js
@@ -21,11 +21,69 @@ function ShowNav(a, b){
 /*столы*/
 
 let content = document.querySelector('.js-content');
+let popup   = document.querySelector('.js-add-visit');
 
 content.addEventListener('click', (data) => {
 	let target = data.target;
 
 	if(target.classList.contains('js-table_cap')){
 		target.classList.add('_none');
+		popup.classList.remove('_none');
+
+		let number_table = target.parentNode.getAttribute('id');
+		number_table = +getString(number_table, number_table.indexOf('_') + 1, number_table.length);
+
+		let table = document.getElementById('number_table');
+		table.innerHTML = +number_table;
 	}
 });
+
+/*Popup*/
+popup.addEventListener('click', (data) => {
+	let target = data.target;
+
+	if(target.classList.contains('js-close_popup')){
+		closePopup();
+	}else if(target.classList.contains('js-enter-date_text')){
+		getDataPopup();
+	}
+});
+
+function closePopup(){
+	let number = document.getElementById('number_table').innerHTML;
+
+	let table = document.querySelector('#table_' + number + ' .js-table_cap');
+	table.classList.remove('_none');
+
+	ClearPopup();
+	popup.classList.add('_none');
+}
+
+function getDataPopup(){
+	let name = document.getElementById('enter_name').value;
+	let time = document.getElementById('enter_time').value;
+
+	ClearPopup();
+
+	let number = document.getElementById('number_table').innerHTML;
+
+	document.querySelector('#table_' + number + ' .js-table__info_name').innerHTML = name;
+	document.querySelector('#table_' + number + ' .js-table__info_time').innerHTML = time;
+
+	popup.classList.add('_none');
+}
+
+function ClearPopup(){
+	document.getElementById('enter_name').value = "";
+	document.getElementById('enter_time').value = "";
+}
+
+function getString(s, pos_st, pos_fn){
+	let st = "";
+
+	for(let i = pos_st; i < pos_fn; i++){
+		st += s[i];
+	}
+
+	return st;
+}
