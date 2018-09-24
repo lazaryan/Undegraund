c0c8d8bbf7399e5744fd99e57c55753a6797a43f
lazaryan <lazaryan99@gmail.com>
2018-09-25 01:00:33 +0300

    fix add hours

--- src/js/table.js
+++ src/js/table.js
@@ -346,14 +346,11 @@ Tabel.prototype = {
 	},
 
 	removeHoursCap () {
-		this.Body.querySelector('#hours_block')
-			.removeChild(this._elements._add_hours);
-
-		this._elements._add_hours = undefined;
+		this._elements._add_hours.style = 'display: none;'
 	},
 
 	addHoursCap () {
-		createElement(this._elements._add_hours_block, this._create.change.setting.elements[0].elements[0], this._elements);
+		this._elements._add_hours.style = '';
 	},
 
 	changeHours () {
